ccad6fc6f65033729e21d13af49b7defb9873802
Witaya252525 <60280369+Witaya252525@users.noreply.github.com>
2023-06-18 15:51:11 +0700

    update_date3
    update  udemy day3

--- Code_Practice/day3u.py
+++ Code_Practice/day3u.py
@@ -1,10 +1,16 @@
-print("welcome to the rollercoaster ")
-height = int(input("What is you height in cm ==>  ?"))
-if height >= 120 :
-    print("You can ride Roaller coaster ")
+# print("welcome to the rollercoaster ")
+# height = int(input("What is you height in cm ==>  ?"))
+# if height >= 120 :
+#     print("You can ride Roaller coaster ")
+
+# else:
+#     print("Sorry you have to grow taller before you can ride")
+    
+print(" Check odd or event number  ")
+number=int(input("Please enter the number "))
+if (number % 2 ) == 0 :
+    print (f' this  = {number} is odd ')
 
 else:
-    print("Sorry you have to grow taller before you can ride")
-    
-
+     print( " This number is event ")
 
