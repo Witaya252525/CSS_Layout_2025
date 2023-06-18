bdbfeec6303d4d47cff7fb60afab438897b304e0
Witaya252525 <60280369+Witaya252525@users.noreply.github.com>
2023-06-18 16:11:47 +0700

    Ticket

---
+++ Code_Practice/dau3U.py
@@ -0,0 +1,34 @@
+# print("welcome to the rollercoaster ")
+# height = int(input("What is you height in cm ==>  ?"))
+# if height >= 120 :
+#     print("You can ride Roaller coaster ")
+
+# else:
+#     print("Sorry you have to grow taller before you can ride")
+    
+# print(" Check odd or event number  ")
+# number=int(input("Please enter the number "))
+# if (number % 2 ) == 0 :
+#     print (f' this  = {number} is odd ')
+
+# else:
+#      print( " This number is event ")
+
+
+
+
+
+print(" Check Height for Roaroller ")
+height = int(input("Please in put height == "))
+if height >= 120 :
+    print(" you can right roller crosster")
+    age = int(input("please in put your age ++ "))
+    if age < 12 :
+        print("Please pay = 12")
+    elif age <=18:
+        print("Please pay = 128")
+    else:
+        print("Please pay = 200")
+
+else:
+    print(" You can not ride Roller couster")
