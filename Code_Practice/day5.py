e89af077683c178c0fa8fd35584f3a174c4c6085
Witaya_Chaison <60280369+Witaya252525@users.noreply.github.com>
2023-06-18 14:09:20 +0700

    Practice.py

---
+++ Code_Practice/day5.py
@@ -0,0 +1,33 @@
+
+
+print("Which charecter are you from 'Advenger?'  ")
+print()
+print("Answer this question and let's find out")
+print()
+print("print Y or No for yes or no ")
+
+
+likeGreen = input("Do you like the color green?: ")
+if likeGreen == "Y":
+  print("You must be the Hulk!")
+else:
+  print("Im aneane")
+
+IronIsCool = input("Do you think building robots is cool?: ")  
+if IronIsCool == "Y":
+  print("You must be Iron Man. Cool suit!")
+else:
+  print("Im batCat")
+
+TimeTravel = input("Do you like traveling through time?: ")  
+if TimeTravel == "Y":
+  print("You must be Captain America!")
+else:
+  print("Im Tureman")
+  
+
+Strong = input("Are you super strong?: ")
+if Strong == "Y":
+  print("You have got to be Thor!")
+else:
+  print("I guess you are not like anyone from 'Avengers.'")
---
+++ Code_Practice/test.py
@@ -0,0 +1,21 @@
+# def ticket(passenger_type):
+#     fare = 50
+#     passenger_type = passenger_type.upper().strip()
+#     if passenger_type == "A" :
+#         fare = 50
+#     elif passenger_type == 'S':
+#         fare = 45
+#     elif passenger_type == 'C' or  passenger_type == 'E' :
+#         fare = 25
+
+# fare = ticket("A")
+# print(fare)
+
+
+
+import statistics
+
+num_list = [1,2,3,4,5]
+avg = statistics.mean(num_list)
+print(avg)
+
