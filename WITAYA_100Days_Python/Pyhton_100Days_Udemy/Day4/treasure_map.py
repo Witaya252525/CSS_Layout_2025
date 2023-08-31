f2c3e1b8d95a5d64d0231171fdae245b719b4911
Witaya252525 <witaya.c@kkumail.com>
2023-08-31 07:45:49 +0700

    aug31_2

--- WITAYA_100Days_Python/Pyhton_100Days_Udemy/Day4/treasure_map.py
+++ WITAYA_100Days_Python/Pyhton_100Days_Udemy/Day4/treasure_map.py
@@ -11,10 +11,13 @@ while True:
    map = [row1 ,row2 ,row3]
    print(f' {row1}\n {row2}\n {row3}\n')
    posittion =input("where do you want to go ==> ")
+
+   # 23 is index position  (0 is first and 1 is second position)
    horizontal = int(posittion[0])
    vertical = int(posittion[1])
 
-   map[vertical-1][horizontal-1] = "90"
+   
+   map[vertical-1][horizontal-1] = "80"
    print(f' {row1}\n {row2}\n {row3}\n')
 
    
